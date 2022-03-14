cf53fba73cc1164d427b3f67b03eb9a96f7454de
TomKan <tomkan0909@gmail.com>
2022-03-14 13:08:51 -0400

    created model for timeseries

--- covidAPI/covidAPI/settings.py
+++ covidAPI/covidAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'time_series.apps.TimeSeriesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'covidAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -105,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
--- covidAPI/time_series/models.py
+++ covidAPI/time_series/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TimeSeries(models.Model):
+    timeseries_name = models.CharField(max_length=1000)
+    province_state = models.CharField(max_length=1000)
+    country_region = models.CharField(max_length=1000)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return "Timeseries: {}, province/state: {}, country/region: {}, latitude: {}, longitude: {}".format(self.timeseries_name,
+                                                                                                            self.province_state, self.country_region, self.latitude, self.longitude)
+
+
+class TimeSeriesData(models.Model):
+    """
+    Represents a single Buoy datapoint
+    """
+    timeseries = models.ForeignKey(TimeSeries, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    cases = models.IntegerField()
+
+    def __str__(self):
+        return "Date: {}, cases: {}".format(self.date, self.cases)
